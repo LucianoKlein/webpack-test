1d49ead640b001a347d3d013e05007c811950f94
wjm <1186619054@qq.com>
2018-01-22 18:07:41 +0800

    测试html-wenpack-plugin

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,8 +1,16 @@
 // webpack --config 配置文件名   指定webpack配置文件
+var htmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-    entry: __dirname + '/src/script/main.js',
+    entry: {
+        main: __dirname + '/src/script/main.js',
+        test: __dirname + '/src/script/test.js'
+    },
     output: {
+        //name hash chunkhash==>可视为文件的版本号，当文件发生改变时，这个值会发生变化
         path: __dirname + '/dist/js',
-        filename: 'bundle.js'
-    }
+        filename: '[name]-[chunkhash].js'
+    },
+    plugins: [
+        new htmlWebpackPlugin()
+    ]
 }
